ae758838e256d27471cef78dc0925efbcb6824e6
claudiobottari <bottari@gmail.com>
2023-07-21 13:51:54 +0200

    some benchmarks

--- README.md
+++ README.md
@@ -4,3 +4,7 @@ DataEnricherLLM is a Python project that aims to enhance the capabilities of Lan
 The main goal of DataEnricherLLM is to provide a reliable and efficient method of enriching a prompt for an LLM. This feature enables a user to receive a more accurate and well-informed response from the LLM, without having to be concerned about response time.
 
 DataEnricherLLM fetches relevant information from the web and provides this data to the LLM. By doing so, the project seeks to significantly improve the quality and relevance of LLM-generated text, and provide users with an overall more satisfactory experience.
+
+## Note
+To make it running it's necessary to create the usual .anv file as follow:
+OPENAI_API_KEY=YOUR_OPEN_AI_KEY
--- data_enricher/benchmark.py
+++ data_enricher/benchmark.py
@@ -42,29 +42,45 @@ class Benchmark():
             os.remove(file_path)
         df.to_excel(file_path, index=False)
 
+    def test_and_retry_benchmark(self):
+        """
+        Sinceh theprocess is long and can often fail due to OpenAI unavailability, let's cycle forever in order to complete the benchmark
+        """
+        while True:
+            try:
+                self.test_benchmark()
+            except:
+                print('Something happened, retrying in 2 minute....')
+                time.sleep(120)
 
     def test_benchmark(self):
         # Collect data
         data = []
         for i, question in enumerate(SampleQuestions.get_list()):
             if self.is_already_done(question):
+                # in order to retry we check if the file with the question was already created
                 print(f'Skip {question} cause it was already done before!')
             else:
                 self.enricher.logger.info(f'{i+1}. {question}')
                 
+                # read the baseline from calling the LLM with just the question
                 baseline = self.get_baseline_answer(question)
                 self.enricher.logger.info(f'{i+1}. BASELINE: {baseline}')
 
+                # call the enrichemetn process
                 enriched, enriched_prompt = self.get_enriched_answer(question)
                 self.enricher.logger.info(f'{i+1}. ENRICHED: {enriched}')
                 
+                # create the dataset
                 data_row = (question, baseline, enriched_prompt, enriched)
                 data.append(data_row)
 
+                # save results to files
                 self.save_file(question, baseline, enriched_prompt, enriched)
                 self.save_xlsx(data)
 
+                # let's wait 2 minute to not add up too many requests to openAI
                 time.sleep(120)
 
 if __name__ == '__main__':
-    Benchmark().test_benchmark()
+    Benchmark().test_and_retry_benchmark()
